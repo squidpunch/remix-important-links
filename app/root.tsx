23ad4145e1cd8ba5b0683ea63f3634e57d252cb7
David Larrabee <dave@larrabeefamily.com>
2021-06-23 23:09:21 -0400

    Disable javascript by default

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,21 @@
 import type { LinksFunction, LoaderFunction } from "remix";
-import { Meta, Links, Scripts, useRouteData, LiveReload } from "remix";
+import {
+  Meta,
+  Links,
+  Scripts,
+  useRouteData,
+  LiveReload,
+  useMatches,
+} from "remix";
 import { Outlet } from "react-router-dom";
-
 import stylesUrl from "./styles/global.css";
+import type { ReactElement } from "react";
 
-export let links: LinksFunction = () => {
+export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export let loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async () => {
   return { date: new Date() };
 };
 
@@ -23,19 +30,24 @@ function Document({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-
-        <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
   );
 }
 
-export default function App() {
-  let data = useRouteData();
+export default function App(): ReactElement {
+  const matches = useMatches();
+
+  // If at least one route wants to hydrate, this will return true
+  const includeScripts = matches.some((match) => match.handle?.hydrate);
+
+  const data = useRouteData();
   return (
     <Document>
       <Outlet />
+      {/* include the scripts, or not! */}
+      {includeScripts && <Scripts />}
       <footer>
         <p>This page was rendered at {data.date.toLocaleString()}</p>
       </footer>
@@ -43,7 +55,7 @@ export default function App() {
   );
 }
 
-export function ErrorBoundary({ error }: { error: Error }) {
+export function ErrorBoundary({ error }: { error: Error }): ReactElement {
   return (
     <Document>
       <h1>App Error</h1>
