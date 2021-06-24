de5753d7020141adde35c80b20a15bb3cd09adcf
David Larrabee <dave@larrabeefamily.com>
2021-06-23 23:00:27 -0400

    Formatting and Linting

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,10 +1,11 @@
 import type { MetaFunction } from "remix";
+import type { ReactElement } from "react";
 
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return { title: "Ain't nothing here" };
 };
 
-export default function FourOhFour() {
+export default function FourOhFour(): ReactElement {
   return (
     <div>
       <h1>404</h1>
